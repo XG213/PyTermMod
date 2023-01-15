b87ecd674b7b9e65561af8478f4895f9f5547d27
Dr. AIT <108739871+totallynotdrait@users.noreply.github.com>
2023-01-15 13:49:49 +0100

    Delete PyTerm directory

--- PyTerm/test.py
+++
@@ -1,8 +0,0 @@
-import subprocess, sys
- 
-while True:
-    term=input("cmd: ")
-
-    if term.__contains__("pip install"):
-        modulename = term.replace("pip install ", "")
-        subprocess.check_call([sys.executable, '-m', 'pip', 'install', modulename])
